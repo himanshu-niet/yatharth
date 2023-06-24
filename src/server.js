cc3f0a19b7b97bf2da028a40272323d20a05037c
Yatharth Dixit <yatharthdixit@Yatharths-MacBook-Air.local>
2023-06-25 00:42:19 +0530

    First Commit

---
+++ src/server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const Note = require("./models/Note")
+
+const bodyParser = require("body-parser");
+const noteRouter = require("./routes/Notes")
+
+app.use(bodyParser.urlencoded({extended : false})); //true : Nested object supported, else not.
+app.use(bodyParser.json());
+
+mongoose.connect("mongodb+srv://yatharthdixit:yathdi1234@cluster0.mqvkeyl.mongodb.net/?retryWrites=true&w=majority").then(function(){
+    app.get('/', function(req, res){
+        var response = {message : "API Working!"}
+        res.json(response) ; 
+    });
+    app.use("/notes", noteRouter);
+    
+});
+
+
+//Home route
+
+const PORT = process.env.PORT || 2222
+app.listen(PORT, function(){
+    console.log("server started at"+PORT);
+});
